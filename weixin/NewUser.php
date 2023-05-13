761d319e7b72ab47533bfc21ff225eb458b89d09
I062552 <lu.liu@sap.com>
2023-05-13 14:59:41 +0800

    fixed an bug

--- weixin/NewUser.php
+++ weixin/NewUser.php
@@ -1,3 +1,4 @@
+<?php
 include_once '../conn.php';
 $suc=initLoginSession();
 if(!$suc){
@@ -5,7 +6,7 @@ if(!$suc){
     return;
 }
 $context=getLoginContext();
-if($context["role"]!="系统管理员")
+if($context["role"]!=="系统管理员")
 {
     print json_encode("不是系统管理员");
     return;
@@ -19,3 +20,4 @@ $suc=AddUser($username,$pwd,$cx);
 $baris = array("suc" => $suc);
 header('Access-Control-Allow-Origin:*');
 print json_encode($baris);
+?>
